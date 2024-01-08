79eee1d6f281b21252797cc486f918f0a75bf1db
Jamzlor <jameslo.dev@gmail.com>
2024-01-08 16:16:33 +0800

    Add reset button and winner display

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ let playerSelection;
 let computerSelection;
 let playerWins = 0;
 let computerWins = 0;
+let ties = 0;
+let winner;
 
+//DOM query selector variables
+const body = document.querySelector('body');
+const playerButton = document.querySelectorAll('.playerChoiceButton');
+const scoreCards = document.querySelectorAll('.scoreCard');
+const huamnScore = document.querySelector('#humanScore');
+const tiedScore = document.querySelector('#tiedScore');
+const computerScore = document.querySelector('#computerScore');
+const resetButton = document.createElement('button');
+
+//DOM manipulations
+resetButton.setAttribute('id', 'resetButton');
 //Logics
 
 function getComputerChoice (){
@@ -21,6 +34,7 @@ function getComputerChoice (){
 function roundLogic(playerChoice, computerChoice){
     
     if(playerChoice === computerChoice){
+        ties++;
         return 'Tied'
     } if(playerChoice === 'rock'){
         if(computerChoice === 'paper'){
@@ -53,18 +67,41 @@ function playOneRound(buttonChoice){
     getComputerChoice();
     playerSelection = buttonChoice.toLowerCase();
     console.log(roundLogic(playerSelection, computerSelection));
-    console.log('Player Win: ' + playerWins);
-    console.log('Computer Win: ' + computerWins);
+    console.log(ties, playerWins, computerWins)
+    tiedScore.innerText = ties;
+    humanScore.innerText = playerWins;
+    computerScore.innerText = computerWins;
 }
 
-//DOM manipulations
-//DOM query selector variables
-const playerButton = document.querySelectorAll('button');
+function checkWins(){
+    if(playerWins === 5 || computerWins === 5){
+        playerButton.forEach((button) => button.setAttribute('disabled', true));
+        (playerWins === 5) ? winner = 'Player Wins!' : winner = 'Computer Wins!';
+        resetButton.innerText = winner + '\
+        Reset Game';
+        body.appendChild(resetButton)
+    };
+}
+
+
+console.log(playerButton)
 
 //Event Listners
 playerButton.forEach((button) => button.addEventListener('click', (e) => {
     playOneRound(e.target.innerText)
+    checkWins();
 }));
+
+resetButton.addEventListener('click', () => {
+    playerWins = 0;
+    computerWins = 0;
+    ties = 0;
+    tiedScore.innerText = ties;
+    humanScore.innerText = playerWins;
+    computerScore.innerText = computerWins;
+    playerButton.forEach((button) => button.removeAttribute('disabled'));
+    body.removeChild(resetButton);
+});
 
 
 
